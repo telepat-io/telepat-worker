1c488f5c5dc454b34c431f9c36adda83e73dd363
Razvan Botea <razvan@appscend.com>
2015-10-09 17:16:55 +0300

    Fixed worker crash when using env variables as config

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ async.series([
 
 		var messageQueueConfig = theWorker.config[theWorker.config.message_queue];
 
-		messageQueueConfig.broadcast = theWorker.broadcast;
+		messageQueueConfig = messageQueueConfig || {broadcast: theWorker.broadcast};
 		var messagingClient = new Models[theWorker.config.message_queue](messageQueueConfig, 'telepat-worker-'+workerType+'-'+workerIndex, workerType);
 		theWorker.setMessagingClient(messagingClient);
 
